560ceb8798d1cc43de01122569fa0b7ad54691bf
Matthew Aliriza (SPF) <Matthew.Aliriza@sanlam.co.za>
2020-07-17 13:03:59 +0200

    Removed '>' from public string as its used for expressions being returned.

--- PageObjects/WebTablePage.cs
+++ PageObjects/WebTablePage.cs
@@ -3,10 +3,10 @@ namespace HackaThon.PageObjects
 {
     public class WebTablePage
     {
-        public static string table => "//table[@class='smart-table table table-striped']";
-        public static string addUserButton => "//button[@class='btn btn-link pull-right']";
-        public static string addUserForm => "//div[@class='modal ng-scope']";
-        public static string tableUsernameValues => "//tr[@ng-repeat='dataRow in displayedCollection']/td[3]";
-        public static string tableFirstLineValues => "//tr[@ng-repeat='dataRow in displayedCollection'][1]/td[not(@class='smart-table-data-cell ng-hide')]";
+        public static string table = "//table[@class='smart-table table table-striped']";
+        public static string addUserButton = "//button[@class='btn btn-link pull-right']";
+        public static string addUserForm = "//div[@class='modal ng-scope']";
+        public static string tableUsernameValues = "//tr[@ng-repeat='dataRow in displayedCollection']/td[3]";
+        public static string tableFirstLineValues = "//tr[@ng-repeat='dataRow in displayedCollection'][1]/td[not(@class='smart-table-data-cell ng-hide')]";
     }
 }
